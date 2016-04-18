779a06faa029dcd75cbcdbb8147cbc57d0710b2f
Daniel Curran <danielcurran@Daniels-MacBook-Pro.local>
2016-04-18 10:55:23 -0500

    added argv to code

--- highlow.php
+++ highlow.php
@@ -1,31 +1,34 @@
 <?php 
 
-$random = mt_rand(0, 100);
-$numberGuess = 0;
+if(is_numeric($argv[1]) && is_numeric($argv[2])){
+    $random = mt_rand($argv[1], $argv[2]);
+    $numberGuess = 0;
 
-do{
-    fwrite(STDOUT, "Guess a number 0 - 100! \n");
-    $guess = trim(fgets(STDIN));
-    $numeric = is_numeric($guess);
-    $numberGuess++;
-    if($guess >= 0 && $guess <= 100 && $numeric){
-        echo "\n";
-
-        if($guess < $random){
-            echo "You guessed {$guess}, the random number is HIGHER! \n";
-        } elseif($guess > $random){
-            echo "You guessed {$guess}, the random number is LOWER! \n";
+    do {
+        fwrite(STDOUT, "Guess a number {$argv[1]} - {$argv[2]}! \n");
+        $guess = trim(fgets(STDIN));
+        $numeric = is_numeric($guess);
+        $numberGuess++;
+        if($argv[1] <= $guess && $guess <= $argv[2] && $numeric){
+            echo "\n";
+            if($guess < $random){
+                echo "You guessed {$guess}, the random number is HIGHER! \n";
+            } elseif($guess > $random){
+                echo "You guessed {$guess}, the random number is LOWER! \n";
+            }
+        } else {
+            echo "You guessed {$guess}, that is not a number between {$argv[1]} and {$argv[2]}, guess again please! \n";
         }
-    } else {
-        echo "You guessed {$guess}, that is not a number between 0 and 100, guess again please! \n";
-    }
 
-} while ($guess != $random);
+    } while ($guess != $random);
 
-echo "GOOD GUESS, YOU GOT IT! \n";
+    echo "GOOD GUESS, YOU GOT IT! \n";
 
-if($numberGuess >= 2){
+    if($numberGuess >= 2){
         echo "It only took you {$numberGuess} guesses! \n";
+    } else {
+        echo "Wow you got it in {$numberGuess} guess! \n";
+    }
 } else {
-    echo "Wow you got it in {$numberGuess} guess! \n";
-}
+        echo "Please enter two integer values \n";
+    }
